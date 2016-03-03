36a0dc00862fa03cedf19bc7ab0d51fc7609e3af
Tom <tputs001@gmail.com>
2016-03-03 10:13:23 -0800

    Added more data into my data.js file.

--- data.js
+++ data.js
@@ -7,7 +7,7 @@ var data = [
       "image": "images/mb.jpg",
       "id": "macbook",
       "keyword": "macbook",
-      "category": ["computers", "computer", "electronics", "laptops"]
+      "category": ["computers", "computer", "electronics", "laptops", "apple"]
     },
     {
       "name": "Macbook Pro",
@@ -15,9 +15,9 @@ var data = [
       "highlights": "Apple MacBook Pro MD101LL/A 13.3-Inch Laptop",
       "description": ["2.5 GHz Dual-Core Intel Core i5 processor", "4 GB DDR3 RAM; 500 GB Hard Drive", "13.3 inch LED-backlit display, 1280-by-800 resolution", "Intel HD Graphics 4000", "Up to 7 hours of wireless web"],
       "image": "images/mbp.jpg",
-      "id": "macbook pro",
-      "keyword": "macbook pro",
-      "category": ["computers", "computer", "electronics", "laptops"]
+      "id": "macbookpro",
+      "keyword": "macbook",
+      "category": ["computers", "computer", "electronics", "laptops", "apple"]
     },
     {
       "name": "Surfacebook Pro",
@@ -25,9 +25,39 @@ var data = [
       "highlights": "Microsoft Surface Book 512 GB, 16 GB RAM, Intel Core i7, NVIDIA GeForce graphics",
       "description": ["13.5-inch PixelSense touchscreen display (3000 x 2000) resolution", "Windows 10 Pro operating system", "Incredibly mobile at 3.48 pounds (1576 grams)", "Surface Pen included"],
       "image": "images/sfb.jpg",
-      "id": "surfacebook pro",
-      "keyword": "surfacebook pro",
-      "category": ["computers", "computer", "electronics", "laptops"]
+      "id": "surfacebookpro",
+      "keyword": "surface",
+      "category": ["computers", "computer", "electronics", "laptops", "microsoft"]
 
-    }
+    },
+    {
+      "name": "Ipad Air",
+      "price": 599,
+      "highlights": "Apple iPad Air 2 MH0W2LL/A 10-Inch Retina Display, 16GB (Gold)",
+      "description": ["Apple iOS 8, 10-Inch Retina Display, 2048x1536 Resolution", "A8X Chip with 64-bit Architecture, M8 Motion Coprocessor", "Wi-Fi (802.11a/b/g/n/ac): 16 GB, Capacity: 2 GB RAM", "8 MP iSight Camera, FaceTime HD Camera", "Up to 10 Hours of Battery Life", "Apple iOS 8, 10-Inch Retina Display, 2048x1536 Resolution"],
+      "image": "images/ipadair.jpg",
+      "id": "ipadair",
+      "keyword": "ipad",
+      "category": ["electronics", "tablets", "apple"]
+    },
+    {
+      "name": "Samsung Galaxy Tab 4",
+      "price": 147.99,
+      "highlights": "Samsung Galaxy Tab 4 (7-Inch, White)",
+      "description": ["Android 4.4 Kit Kat OS, 1.2 GHz quad-core processor", "8 GB Flash Memory, 1.5 GB RAM Memory", "WXGA Display (1280x800 Resolution)", "32GB of memory available through a microSD slot and 50GB of free Dropbox storage", "Wi-Fi Tablet, Android 4.4 Kit Kat OS, 1.2 GHz quad-core processor", "8 GB Internal Memory, 1.5 GB RAM Memory"],
+      "image": "images/galaxytab4.jpg",
+      "id": "galaxytab4",
+      "keyword": "samsung",
+      "category": ["electronics", "tablets", "samsung"]
+    },
+    // {
+    //   "name": "Ipad Air",
+    //   "price": 599,
+    //   "highlights": "Apple iPad Air 2 MH0W2LL/A 10-Inch Retina Display, 16GB (Gold)",
+    //   "description": ["Apple iOS 8, 10-Inch Retina Display, 2048x1536 Resolution", "A8X Chip with 64-bit Architecture, M8 Motion Coprocessor", "Wi-Fi (802.11a/b/g/n/ac): 16 GB, Capacity: 2 GB RAM", "8 MP iSight Camera, FaceTime HD Camera", "Up to 10 Hours of Battery Life", "Apple iOS 8, 10-Inch Retina Display, 2048x1536 Resolution"],
+    //   "image": "images/ipadair.jpg",
+    //   "id": "ipadair",
+    //   "keyword": "ipad air",
+    //   "category": ["computers", "computer", "electronics", "tablets"]
+    // }
 ];
